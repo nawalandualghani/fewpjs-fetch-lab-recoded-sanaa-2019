9305ff96b9ca021049951ae7afd6c743eb1ecb7a
nawal abdualghani <nawalabdualghani@gmail.com>
2019-11-18 05:54:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function fetchBooks() {
-  fetch('https://anapioficeandfire.com/api/books')
+  fetch("https://anapioficeandfire.com/api/books")
     .then(resp => resp.json())
     .then(json => console.log(json));
 }
